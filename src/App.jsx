c8f2367e4d5c35af9a5ea979f51a385f48d86ca2
Hulio <julio.ferrero.mp@gmail.com>
2023-04-06 18:35:28 +0200

    🔨 Hot fix for url image (3).

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "@fontsource/syne/800.css"
 import './App.css'
 import Nav from "./components/Nav/Nav.jsx";
+import CatEmoji from './assets/blushing_swearing_angry_cat_face.png'
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
         <Nav/>
         <div className="header">
             <div className="header_spacer"/>
-            <img alt="emoji cat" src="./assets/blushing_swearing_angry_cat_face.png"/>
+            <img alt="emoji cat" src={CatEmoji}/>
             <h1 style={{fontWeight: 800}}>Cursed Cat</h1>
             <p>"The real danger is not that computers will begin to think like men, but that men will begin to think like computers."</p>
         </div>
